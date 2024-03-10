558e28a08d859b3049242d0ff64bb24a703e4879
Waithera Macharia <joycemacharia02@gmail.com>
2024-03-10 20:08:54 +0300

    Add functions to pages

--- pages/02_Dashboard.py
+++ pages/02_Dashboard.py
@@ -1,9 +1,216 @@
-import streamlit as st 
+import streamlit as st
+import pandas as pd
+import pyodbc
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Page title and icon
 st.set_page_config(
-    page_title='Dashboard',
-    page_icon=':)',
-    layout='wide'
-
+    page_title="Dashboard",
+    page_icon=":chart_with_upwards_trend:"
 )
-st.title('Dashboard')
+
+st.cache_resource(show_spinner='connecting to Database.....')
+def initialize_connection():
+    connection = pyodbc.connect(
+        "DRIVER={SQL Server};SERVER="
+        + st.secrets['SERVER']
+        + ";DATABASE="
+        + st.secrets['DATABASE']
+        + ";UID="
+        + st.secrets['UID']
+        + ";PWD="
+        + st.secrets['PWD']
+    )
+    return connection
+
+conn = initialize_connection()
+
+# Function to execute SQL query and return DataFrame
+@st.cache_data()
+def query_database(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
+        df = pd.DataFrame.from_records(data=rows, columns=[column[0] for column in cur.description])
+    return df
+
+# Function to load dataset from file
+def load_dataset(file_path):
+    df = pd.read_csv(file_path) if file_path.endswith('.csv') else pd.read_excel(file_path)
+    return df
+
+# Function to display visualizations for each feature
+def display_visualizations(data):
+    st.subheader('Visualizations')
+
+    # Set style
+    sns.set_style('whitegrid')
+
+    # Create columns layout
+    col1, col2 = st.columns(2)
+
+    # Chart for Gender
+    with col1:
+        st.write("### Gender Distribution")
+        gender_counts = data['gender'].value_counts()
+        plt.figure(figsize=(10, 6))
+        gender_plot = sns.barplot(x=gender_counts.index, y=gender_counts.values, palette='pastel')
+        gender_plot.set_title('Gender Distribution')
+        gender_plot.set_xlabel('Gender')
+        gender_plot.set_ylabel('Count')
+        st.pyplot(fig=gender_plot.figure)
+
+    # Chart for Tenure
+    with col2:
+        st.write("### Tenure Distribution")
+        plt.figure(figsize=(10, 6))
+        tenure_plot = sns.histplot(data['tenure'], bins=20, kde=True, color='skyblue')
+        tenure_plot.set_title('Tenure Distribution')
+        tenure_plot.set_xlabel('Tenure')
+        tenure_plot.set_ylabel('Frequency')
+        st.pyplot(fig=tenure_plot.figure)
+
+    # Chart for Contract
+    with col1:
+        st.write("### Contract Distribution")
+        contract_counts = data['Contract'].value_counts()
+        plt.figure(figsize=(10, 6))
+        contract_plot = sns.barplot(x=contract_counts.index, y=contract_counts.values, palette='pastel')
+        contract_plot.set_title('Contract Distribution')
+        contract_plot.set_xlabel('Contract Type')
+        contract_plot.set_ylabel('Count')
+        st.pyplot(fig=contract_plot.figure)
+
+    # Chart for Payment Method
+    with col2:
+        st.write("### Payment Method Distribution")
+        payment_counts = data['PaymentMethod'].value_counts()
+        plt.figure(figsize=(10, 6))
+        payment_plot = sns.barplot(x=payment_counts.index, y=payment_counts.values, palette='pastel')
+        payment_plot.set_title('Payment Method Distribution')
+        payment_plot.set_xlabel('Payment Method')
+        payment_plot.set_ylabel('Count')
+        payment_plot.set_xticklabels(payment_plot.get_xticklabels(), rotation=45, horizontalalignment='right')
+        st.pyplot(fig=payment_plot.figure)
+
+    # Chart for Monthly Charges
+    with col1:
+        st.write("### Monthly Charges Distribution")
+        plt.figure(figsize=(10, 6))
+        monthly_charges_plot = sns.histplot(data['MonthlyCharges'], bins=20, kde=True, color='lightgreen')
+        monthly_charges_plot.set_title('Monthly Charges Distribution')
+        monthly_charges_plot.set_xlabel('Monthly Charges')
+        monthly_charges_plot.set_ylabel('Frequency')
+        st.pyplot(fig=monthly_charges_plot.figure)
+
+    # Chart for Total Charges
+    with col2:
+        st.write("### Total Charges Distribution")
+        total_charges_numeric = pd.to_numeric(data['TotalCharges'], errors='coerce').dropna()
+        plt.figure(figsize=(10, 6))
+        total_charges_plot = sns.histplot(total_charges_numeric, bins=20, kde=True, color='salmon')
+        total_charges_plot.set_title('Total Charges Distribution')
+        total_charges_plot.set_xlabel('Total Charges')
+        total_charges_plot.set_ylabel('Frequency')
+        st.pyplot(fig=total_charges_plot.figure)
+
+    senior_citizens_data = data[data['SeniorCitizen'] == 1]
+
+    if 'Churn' in senior_citizens_data.columns:
+        # Map 'True' and 'False' to 'Yes' and 'No' for the 'Churn' column
+        senior_citizens_data['Churn'] = senior_citizens_data['Churn'].map({True: 'Yes', False: 'No'})
+
+        # Count the number of churns (Yes or No) among Senior Citizens
+        senior_citizens_churn_counts = senior_citizens_data['Churn'].value_counts()
+
+        # Visualization
+        st.write("### Churn Distribution Among Senior Citizens")
+        plt.figure(figsize=(10, 6))
+        churn_plot = sns.barplot(x=senior_citizens_churn_counts.index, y=senior_citizens_churn_counts.values,
+                                palette='pastel')
+        churn_plot.set_title('Churn Distribution Among Senior Citizens')
+        churn_plot.set_xlabel('Churn')
+        churn_plot.set_ylabel('Count')
+        st.pyplot(churn_plot.figure)
+
+    with col2:
+        if 'Churn' in data.columns:
+            data['Churn'] = data['Churn'].map({True: 'Yes', False: 'No'})
+
+            # Count the number of churns (Yes or No) for the entire dataset
+            churn_counts = data['Churn'].value_counts()
+
+            # Visualization as a pie chart
+            fig, ax = plt.subplots()
+            ax.pie(churn_counts, labels=churn_counts.index, autopct='%1.1f%%', startangle=90,
+                   colors=['#ff9999', '#66b3ff'])
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+            plt.title(' Churn Distribution')
+
+            # Display the plot
+            st.pyplot(fig)
+
+# Function to perform Exploratory Data Analysis (EDA)
+def perform_eda(data):
+    st.subheader('Exploratory Data Analysis (EDA)')
+
+# Function to calculate Key Performance Indicators (KPIs)
+def calculate_kpis(data):
+    st.subheader('Key Performance Indicators (KPIs)')
+
+    # Check if 'Churn' column exists
+    if 'Churn' in data.columns:
+        # Calculate KPIs
+        total_customers = len(data)
+        average_tenure = data['tenure'].mean()
+        total_monthly_charges = data['MonthlyCharges'].sum()
+        churned_customers = len(data[data['Churn'] == 'Yes'])
+        churn_rate = (churned_customers / total_customers) * 100
+        total_revenue = pd.to_numeric(data['TotalCharges'], errors='coerce').sum()
+        average_revenue = total_revenue / total_customers
+
+        # Create a DataFrame to hold KPIs
+        kpi_data = {
+            'KPI Name': ['Total Customers', 'Average Tenure', 'Total Monthly Charges', 'Churned Customers', 'Churn Rate',
+                         'Total Revenue', 'Average Revenue'],
+            'Value': [total_customers, f"{average_tenure:.2f}", f"${total_monthly_charges:.2f}", churned_customers,
+                      f"{churn_rate:.2f}", f"${total_revenue:.2f}", f"${average_revenue:.2f}"]
+        }
+        kpi_df = pd.DataFrame(kpi_data)
+
+        st.table(kpi_df)
+    else:
+        st.warning("Churn column not found in the dataset.")
+
+# Load data from the database
+conn = initialize_connection()
+
+# Title of the dashboard
+st.title('Churn Analysis')
+
+# Add selectbox to choose between EDA and KPIs
+selected_analysis = st.selectbox('Select Analysis Type', ['Exploratory Data Analysis (EDA)', 'Key Performance Indicators (KPIs)'])
+
+# Add selectbox to choose dataset
+selected_dataset = st.selectbox('Select Dataset',
+                                ['LP2_Telco_churn_first_3000', 'Telco-churn-second-2000.xlsx', 'LP2_Telco-churn-last-2000.csv'])
+
+if selected_dataset == 'LP2_Telco_churn_first_3000':
+    # Load data from the first dataset
+    data = query_database(
+        "SELECT gender, tenure, Contract, PaymentMethod, SeniorCitizen, Churn, MonthlyCharges, TotalCharges FROM LP2_Telco_churn_first_3000")
+else:
+    # Load data from the selected file
+    file_path = f"data/{selected_dataset}"
+    data = load_dataset(file_path)
+
+# Perform the selected analysis
+if selected_analysis == 'Exploratory Data Analysis (EDA)':
+    perform_eda(data)
+else:
+    calculate_kpis(data)
+
+# Display visualizations (always shown regardless of the selected analysis)
+display_visualizations(data)
---
+++ pages/03_predict.py
@@ -0,0 +1,165 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import numpy as np
+import os
+import datetime
+
+st.set_page_config(
+    page_title='Predict',
+    page_icon=':)',
+    layout='wide'
+)
+
+@st.cache_resource(show_spinner='Loading models')
+def decision_tree_pipeline():
+    pipeline = joblib.load('./models/Decision Tree_model.joblib')
+    return pipeline
+
+ 
+@st.cache_resource(show_spinner='Loading models')
+def random_forest_pipeline(): 
+    pipeline = joblib.load('./models/Random Forest_model.joblib')  
+    return pipeline
+
+
+def choose_model():
+    
+    column1, column2 = st.columns(2)
+    
+    with column1:
+        st.selectbox ('Please select a model', options=['Decision Tree Model', 'Random Forest Model'],key= 'selected_model')    
+    with column2:
+        pass
+    
+    if st.session_state['selected_model'] == 'Decision Tree Model':
+        pipeline = decision_tree_pipeline()
+    else:
+        pipeline = random_forest_pipeline()
+        
+    encoder = joblib.load('./models/encoder.joblib')
+
+    return pipeline, encoder
+
+
+
+if 'prediction' not in st.session_state:
+    st.session_state['prediction'] = None  
+            
+if 'probability' not in st.session_state:
+    st.session_state['probability'] = None
+
+
+def make_prediction(pipeline, encoder):
+    gender = st.session_state['gender']
+    SeniorCitizen = st.session_state['SeniorCitizen']
+    Partner = st.session_state['Partner']
+    Dependents = st.session_state['Dependents']
+    tenure = st.session_state['tenure']
+    PhoneService = st.session_state['PhoneService']
+    MultipleLines = st.session_state['MultipleLines']
+    InternetService = st.session_state['InternetService']
+    OnlineSecurity = st.session_state['OnlineSecurity']
+    OnlineBackup = st.session_state['OnlineBackup']
+    DeviceProtection = st.session_state['DeviceProtection']
+    TechSupport = st.session_state['TechSupport']
+    StreamingTV = st.session_state['StreamingTV']
+    StreamingMovies = st.session_state['StreamingMovies']
+    Contract = st.session_state['Contract']
+    PaperlessBilling = st.session_state['PaperlessBilling']
+    PaymentMethod = st.session_state['PaymentMethod']
+    MonthlyCharges = st.session_state['MonthlyCharges']
+    TotalCharges = st.session_state['TotalCharges']
+    
+    
+    columns = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure','PhoneService', 'MultipleLines',
+               'InternetService', 'OnlineSecurity','OnlineBackup', 'DeviceProtection', 'TechSupport', 
+               'StreamingTV','StreamingMovies', 'Contract', 'PaperlessBilling', 
+               'PaymentMethod','MonthlyCharges', 'TotalCharges']
+    
+    data = [[gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService,MultipleLines, InternetService, OnlineSecurity,OnlineBackup,DeviceProtection, TechSupport, StreamingTV,StreamingMovies, Contract, PaperlessBilling, PaymentMethod,MonthlyCharges, TotalCharges]]
+    
+    # Create a dataframe
+    df = pd.DataFrame(data, columns=columns)
+    
+     
+    df ['Prediction time'] = datetime.date.today()
+    df ['Model Used'] = st.session_state['selected_model']
+    
+    df.to_csv('./data/history.csv', mode = 'a', header =not os.path.exists('./data/history.csv'), index=False)
+   
+    #Make prediction
+    pred = pipeline.predict(df) 
+    pred = int(pred[0])
+    prediction = encoder.inverse_transform([pred])
+    
+    # Get probabilities
+    probability = pipeline.predict_proba(df)
+    
+    # updating state
+    st.session_state['prediction'] = prediction
+    st.session_state['probability'] = probability
+    
+    return prediction,probability
+
+def info_form():
+    
+    pipeline, encoder = choose_model()
+
+    with st.form('input feature'):
+        
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.write('### Personal Details')
+            # Add input fields and store values in input_features dictionary
+            st.selectbox('Select Gender:', options=['Male','Female'], key='gender')
+            st.selectbox('SeniorCitizen:', options=['Yes', 'No'], key='SeniorCitizen')
+            st.selectbox('Partner:', options=['Yes','No'], key='Partner')
+            st.selectbox('Dependents:', options=['Yes','No'], key='Dependents')
+            st.number_input('Tenure:', min_value= 0, max_value = 71, step=1, key='tenure')
+            
+
+        with col2:
+            st.write('### Services Subscribed to')
+            # Subscriptions input fields
+            st.selectbox('PhoneService:', options=['Yes','No'], key='PhoneService')
+            st.selectbox('MultipleLines:', options=['Yes','No'], key='MultipleLines')
+            st.selectbox('InternetService:',options=['Yes','No'],key='InternetService')
+            st.selectbox('OnlineSecurity:',options=['Yes','No'],key='OnlineSecurity')
+            st.selectbox('OnlineBackup:',options=['Yes','No'],key='OnlineBackup')
+            st.selectbox('DeviceProtection:',options=['Yes','No'],key='DeviceProtection')
+            st.selectbox('TechSupport:',options=['Yes','No'],key='TechSupport')
+            st.selectbox('StreamingTV:',options=['Yes','No'],key='StreamingTV')
+            st.selectbox('StreamingMovies:',options=['Yes','No'],key='StreamingMovies')
+            
+
+        with col3:
+            st.write('### Payment Options')
+            # Input fields for payment methods
+            st.selectbox('Contract:', options=['Month-to-month','Two year','One year'], key='Contract')
+            st.selectbox('PaperlessBilling:', options=['Yes','No'], key='PaperlessBilling')
+            st.selectbox('PaymentMethod:', options=['Electronic check','Mailed check','Bank transfer (automatic)','Credit card (automatic)'], key='PaymentMethod')
+            st.number_input('MonthlyCharges:',min_value= 0,  key='MonthlyCharges')
+            st.number_input('TotalCharges:', min_value =0, key='TotalCharges')
+            
+        st.form_submit_button('Predict', on_click=make_prediction, kwargs=dict(pipeline = pipeline, encoder = encoder))
+        
+        
+if __name__ == '__main__':
+    st.title('Predictor')
+    info_form()
+    
+    prediction = st.session_state['prediction']
+    probability = st.session_state['probability']  
+         
+    if not prediction:
+        st.markdown('### Prediction will show here')
+    elif prediction == 'Yes':
+        probability_of_true = probability[0][0] * 100        
+        st.markdown(f'### Customer will churn with a probability of:{round(probability_of_true, 2)}%')
+    else:
+        probability_of_false = probability[0][1] * 100 
+        st.markdown(f'### Customer will not churn with a probability of:{round (probability_of_false,2)}%')
+
+    st.write(st.session_state)
